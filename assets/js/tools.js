7bc7262d17edfa4500d98778ff41956a003f2b0a
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-07-04 00:28:52 +0200

    Change `includeHTML` function

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,20 +1,26 @@
-function includeHTML(file, elementId) {
-  fetch(file)
-    .then(response => {
-      if (response.ok) {
-        return response.text();
-      }
-      throw new Error('Network response was not ok.');
-    })
-    .then(html => {
-      const element = document.getElementById(elementId);
-      if (element) {
-        element.innerHTML = html;
-      } else {
-        throw new Error(`Element with ID '${elementId}' not found.`);
-      }
-    })
-    .catch(error => {
-      console.error('Error including HTML:', error);
-    });
+function includeHTML(filesAndIds) {
+  const promises = filesAndIds.map(({ file, elementId }) =>
+    fetch(file)
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        }
+        throw new Error(`Network response was not ok for file: ${file}`);
+      })
+      .then(html => {
+        const element = document.getElementById(elementId);
+        if (element) {
+          const div = document.createElement('div');
+          div.innerHTML = html;
+          element.appendChild(div);
+        } else {
+          throw new Error(`Element with ID '${elementId}' not found.`);
+        }
+      })
+      .catch(error => {
+        console.error('Error including HTML:', error);
+      })
+  );
+
+  return Promise.all(promises);
 }
