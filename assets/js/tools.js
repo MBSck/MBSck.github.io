163167389d76f6b43b63212737062765a7079f95
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-07-04 00:48:41 +0200

    Test javascript

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,3 +1,22 @@
+function calculateAge(birthday) {
+  const birthDate = new Date(birthday);
+  const today = new Date();
+  
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  
+  return age;
+}
+
+function getCurrentYear() {
+  const currentYear = new Date().getFullYear();
+  return currentYear;
+}
+
 function includeHTML(filesAndIds) {
   const promises = filesAndIds.map(({ file, elementId }) =>
     fetch(file)
