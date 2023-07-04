c5e6ba556620dc421b229f9e95168fee4f2bb5a9
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-07-04 13:30:36 +0200

    Fix smaller errors

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,17 +1,3 @@
-function calculateAge(birthday) {
-  const birthDate = new Date(birthday);
-  const today = new Date();
-  
-  let age = today.getFullYear() - birthDate.getFullYear();
-  const monthDiff = today.getMonth() - birthDate.getMonth();
-  
-  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
-  }
-  
-  return age;
-}
-
 function getCurrentYear() {
   const currentYear = new Date().getFullYear();
   return currentYear;
